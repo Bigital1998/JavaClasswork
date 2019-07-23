f884c82a4aa3258d075ab0a701f6a5553b5fde55
Bigital1998 <brandonmatara2019@gmail.com>
2019-07-23 09:27:32 +0300

    first commit

---
+++ demo1.java
@@ -0,0 +1,7 @@
+// //single line commments in java
+// //Java is a general-purpose programming
+public class demo1{
+	public static void main(String[] args){
+		System.out.print("Hello world");
+	}
+}
